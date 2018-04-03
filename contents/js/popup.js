9e11777047546798fbd70a7e75cf892e42925268
Will Taylor <taylorw@udel.edu>
2018-04-03 10:12:38 -0400

    Ridiculousness

--- contents/js/popup.js
+++ contents/js/popup.js
@@ -1,18 +1,31 @@
 $(document).ready(function(){
-  $("#setEnvironment").submit(setDefaultEnvironment);
-  $('#shortcuts').click(function(e){
-    e.preventDefault();
-    chrome.tabs.create({ url: "chrome://extensions/shortcuts" });
-  });
 
-  $('#lock-env').click(function(e){
-    var isLocked = $('#lock-env').prop('checked');
-    var request = new Request(Request.Type.SET_LOCK, {lock: isLocked});
-    chrome.runtime.sendMessage(request.serialize(), function(response){
-      console.log("Lock set to", isLocked, $('#lock-env').val());
+  ;(function(){
+    $("#setEnvironment").submit(setDefaultEnvironment);
+    $('#shortcuts').click(function(e){
+      e.preventDefault();
+      chrome.tabs.create({ url: "chrome://extensions/shortcuts" });
     });
-  })
 
+    $('#lock-env').click(function(e){
+      var isLocked = $('#lock-env').prop('checked');
+      var request = new Request(Request.Type.SET_LOCK, {lock: isLocked});
+      chrome.runtime.sendMessage(request.serialize(), function(response){
+        console.log("Lock set to", isLocked, $('#lock-env').val());
+      });
+    });
+
+    $('.qol-menu-tab').click(function(e){
+      $('.qol-menu-tab').removeClass("active");
+      $(this).addClass("active");
+
+      var tab = $(this).text();
+      $('.qol-section').hide();
+      $('div[name="'+tab+'"]').show();
+    });
+
+    $('#appian-tabs-tab').click();
+  })();
 
   chrome.storage.sync.get('default_env', function(result){
     if(result.default_env){
